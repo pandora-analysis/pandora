908ef8225f922b7b628b7b3c046cf391d8da469c
Raphaël Vinot <raphael@vinot.info>
2022-08-29 16:38:33 +0200

    chg: Autorestart the unoserver process if it breaks
    Related #93

--- bin/unoserver_launcher.py
+++ bin/unoserver_launcher.py
@@ -2,6 +2,8 @@
 
 import logging
 
+from datetime import datetime, timedelta
+
 from unoserver.server import UnoServer  # type: ignore
 
 from pandora.default import AbstractManager
@@ -15,17 +17,34 @@ class UnoserverLauncher(AbstractManager):
     def __init__(self, loglevel: int=logging.INFO):
         super().__init__(loglevel)
         self.script_name = 'unoserver'
-        self.process = self._launch_unoserver()
-        self.set_running()
 
     def _launch_unoserver(self):
         unoserver = UnoServer()
-        return unoserver.start()
+        return unoserver.start(), datetime.now()
+
+    def safe_run(self):
+        # it sometimes fails but simply restarting the server fixes it
+        self.process, start_time = self._launch_unoserver()
+        self.set_running()
+        retry = 0
+        while True:
+            self.run(sleep_in_sec=10)
+            if self.shutdown_requested():
+                break
+            if retry >= 3:
+                self.logger.critical(f'Unable to restart {self.script_name}.')
+                break
+            if datetime.now() - start_time > timedelta(seconds=60):
+                retry = 0
+            else:
+                retry += 1
+            self.process, start_time = self._launch_unoserver()
+            self.set_running()
 
 
 def main():
     u = UnoserverLauncher()
-    u.run(sleep_in_sec=10)
+    u.safe_run()
 
 
 if __name__ == '__main__':
