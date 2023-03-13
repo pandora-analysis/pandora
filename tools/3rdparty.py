8f1337e5b229ee4b2044e763bcd77ba38cc36ab6
Raphaël Vinot <raphael@vinot.info>
2023-03-13 19:10:26 +0100

    fix: Make bandit happy

--- tools/3rdparty.py
+++ tools/3rdparty.py
@@ -19,47 +19,47 @@ if __name__ == '__main__':
     dest_dir_font = get_homedir() / 'website' / 'web' / 'static' / 'font' / 'lib'
     safe_create_dir(dest_dir_font)
 
-    jquery = requests.get(f'https://code.jquery.com/jquery-{jquery_version}.min.js')
+    jquery = requests.get(f'https://code.jquery.com/jquery-{jquery_version}.min.js', timeout=5)
     with (dest_dir_js / 'jquery.min.js').open('wb') as f:
         f.write(jquery.content)
         print(f'Downloaded jquery v{jquery_version}.')
 
-    moments = requests.get(f'https://cdnjs.cloudflare.com/ajax/libs/moment.js/{moments_version}/moment-with-locales.min.js')
+    moments = requests.get(f'https://cdnjs.cloudflare.com/ajax/libs/moment.js/{moments_version}/moment-with-locales.min.js', timeout=5)
     with (dest_dir_js / 'moment-with-locales.min.js').open('wb') as f:
         f.write(moments.content)
         print(f'Downloaded moments v{moments_version}.')
 
-    chart = requests.get(f'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/{chart_version}/chart.min.js')
+    chart = requests.get(f'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/{chart_version}/chart.min.js', timeout=5)
     with (dest_dir_js / 'chart.min.js').open('wb') as f:
         f.write(chart.content)
         print(f'Downloaded chart v{chart_version}.')
 
-    datepicker_js = requests.get(f'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/{datepicker_version}/js/bootstrap-datepicker.min.js')
+    datepicker_js = requests.get(f'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/{datepicker_version}/js/bootstrap-datepicker.min.js', timeout=5
     with (dest_dir_js / 'bootstrap-datepicker.min.js').open('wb') as f:
         f.write(datepicker_js.content)
         print(f'Downloaded datepicker js v{datepicker_version}.')
 
-    datepicker_css = requests.get(f'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/{datepicker_version}/css/bootstrap-datepicker.min.css')
+    datepicker_css = requests.get(f'https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/{datepicker_version}/css/bootstrap-datepicker.min.css', timeout=5
     with (dest_dir_css / 'bootstrap-datepicker.min.css').open('wb') as f:
         f.write(datepicker_css.content)
         print(f'Downloaded datepicker css v{datepicker_version}.')
 
-    dropzone_js = requests.get(f'https://unpkg.com/dropzone@{dropzone_version}/dist/dropzone-min.js')
+    dropzone_js = requests.get(f'https://unpkg.com/dropzone@{dropzone_version}/dist/dropzone-min.js', timeout=5
     with (dest_dir_js / 'dropzone-min.js').open('wb') as f:
         f.write(dropzone_js.content)
         print(f'Downloaded dropzone js v{dropzone_version}')
 
-    dropzone_css = requests.get(f'https://unpkg.com/dropzone@{dropzone_version}/dist/dropzone.css')
+    dropzone_css = requests.get(f'https://unpkg.com/dropzone@{dropzone_version}/dist/dropzone.css', timeout=5
     with (dest_dir_css / 'dropzone.css').open('wb') as f:
         f.write(dropzone_css.content)
         print(f'Downloaded dropzone css v{dropzone_version}')
 
-    datatables_css = requests.get(f'https://cdn.datatables.net/{datatables_version}/css/jquery.dataTables.min.css')
+    datatables_css = requests.get(f'https://cdn.datatables.net/{datatables_version}/css/jquery.dataTables.min.css', timeout=5)
     with (dest_dir_css / 'datatables.css').open('wb') as f:
         f.write(datatables_css.content)
         print(f'Downloaded datatables css v{datatables_version}')
 
-    datatables_js = requests.get(f'https://cdn.datatables.net/{datatables_version}/js/jquery.dataTables.min.js')
+    datatables_js = requests.get(f'https://cdn.datatables.net/{datatables_version}/js/jquery.dataTables.min.js', timeout=5)
     with (dest_dir_js / 'datatables.js').open('wb') as f:
         f.write(datatables_js.content)
         print(f'Downloaded datatables js v{datatables_version}')
