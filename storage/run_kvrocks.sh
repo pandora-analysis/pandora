10736956d5c6e9cf5484684d5afe5eb153dea867
Raphaël Vinot <raphael@vinot.info>
2025-03-12 16:39:47 +0100

    new: Support system-wide kvrocks install

--- storage/run_kvrocks.sh
+++ storage/run_kvrocks.sh
@@ -5,9 +5,11 @@ set -x
 
 if [ -f ../../kvrocks/build/kvrocks ]; then
     ../../kvrocks/build/kvrocks -c kvrocks.conf
+elif [ -x "$(command -v kvrocks)" ]; then
+    echo 'kvrocks does not seem to be built locally, using the system-wide install instead.'
+    kvrocks -c kvrocks.conf
 else
-    echo 'kvrocks does not seem to be installed locally, using docker instead.'
-    echo 'We assume you have docker installed in rootless mode, following the instructions here: https://docs.docker.com/engine/security/rootless/'
-    echo 'If you have docker installed in normal mode, add sudo in front of the command below'
-    docker run -d -it -p6101:6101 -v ./:/kvrocks/conf:rw apache/kvrocks --bind 0.0.0.0 --config /kvrocks/conf/kvrocks.conf
+    echo 'kvrocks does not seem to be installed, please install kvrocks and try again.'
+    echo 'You can get the DEB package from https://github.com/RocksLabs/kvrocks-fpm/releases'
+    exit 1
 fi
