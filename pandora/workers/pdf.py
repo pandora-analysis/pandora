80dafe4e709035fdcb47c78744d7210b55808bfa
Dustin Gawron <d.gawron@uni-muenster.de>
2025-09-02 16:48:12 +0200

    chg: Improve PDF analysis

--- pandora/workers/pdf.py
+++ pandora/workers/pdf.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 
 import fitz  # type: ignore[import-untyped]
+import re
 
 from ..helpers import Status
 from ..task import Task
@@ -56,9 +57,10 @@ class Pdf(BaseWorker):
             doc = fitz.open(file_path)
             for xref in range(1, doc.xref_length()):
                 obj_dict = doc.xref_object(xref, compressed=True)
-                # Check for /AA and /OpenAction
-                for keyword in ["/AA", "/OpenAction"]:
-                    if keyword in obj_dict:
+                # Check for /AA, /OpenAction OR /Launch
+                for keyword in ["/AA", "/OpenAction", "/Launch"]:
+                    # Use regex to reduce FPs, especially with /AA
+                    if len(re.findall(keyword + '(?![A-Za-z])', obj_dict)) > 0:
                         suspicious_objects.append(f'{keyword} found in object {xref}: {obj_dict}')
                         # Attempt to extract the object content if possible
                         try:
